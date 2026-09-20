08932259c4e50c7c4f4eeef820179bde7ed45c3d
Alex Frolov <fr.280907@gmail.com>
2026-09-20 14:37:58 +0200

    commit

--- src/components/TrendingMovieGrid/TrendingMovieGrid.tsx
+++ src/components/TrendingMovieGrid/TrendingMovieGrid.tsx
@@ -1,12 +1,12 @@
-import css from './TrendingMovieGrid.module.css';
-import type { Movie } from '../../types/movie';
-import Container from '../Container/Container';
 import { useQuery } from '@tanstack/react-query';
-import { fetchFirstMovies } from '../../services/movieService';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Scrollbar } from 'swiper/modules';
+import { fetchFirstMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+import css from './TrendingMovieGrid.module.css';
+import Container from '../Container/Container';
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
 interface TrendingMovieProps {
   onSelect: (movie: Movie) => void;
@@ -21,35 +21,36 @@ export default function TrendingMovieGrid({ onSelect }: TrendingMovieProps) {
   return (
     <section className={css.trendingMovie}>
       <Container>
-        <h2 className={css.title}>Trending</h2>
-        <Swiper
-          spaceBetween={16}
-          slidesPerView={5}
-          slidesPerGroup={5}
-          modules={[Navigation]}
-          navigation
-        >
-          {data?.results.map((movie) => (
-            <SwiperSlide
-              onClick={() => onSelect(movie)}
-              key={movie.id}
-              className={css.item}
-            >
-              <div className={css.card}>
-                <img
-                  className={css.image}
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                  loading="lazy"
-                />
-                <h2 className={css.subtitle}>{movie.title}</h2>
-                <p
-                  className={css.average}
-                >{`${movie.vote_average.toFixed(1)} / 10`}</p>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div className={css.container}>
+          <h2 className={css.title}>Trending</h2>
+          <Swiper
+            spaceBetween={20}
+            slidesPerView={6.5}
+            modules={[Scrollbar]}
+            scrollbar={{ draggable: true }}
+          >
+            {data?.results.map((movie) => (
+              <SwiperSlide
+                onClick={() => onSelect(movie)}
+                key={movie.id}
+                className={css.item}
+              >
+                <div className={css.card}>
+                  <img
+                    className={css.image}
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                    loading="lazy"
+                  />
+                  <h2 className={css.subtitle}>{movie.title}</h2>
+                  <p
+                    className={css.average}
+                  >{`${movie.vote_average.toFixed(1)} / 10`}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </Container>
     </section>
   );
